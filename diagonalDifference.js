2fb50b11ce009e23aa342056ee6ad9ca23402d56
Facundo Vergara <fakumax@gmail.com>
2022-10-23 11:36:28 -0300

    First

---
+++ diagonalDifference.js
@@ -0,0 +1,23 @@
+function diagonalDifference(arr) {
+  // Write your code here
+  let suma = 0;
+  let resta = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length; j++) {
+      console.log(arr[i][j]);
+      if (i === j) {
+        suma += arr[i][j];
+      }
+      if (i + j === arr.length - 1) {
+        resta += arr[i][j];
+      }
+    }
+  }
+  return Math.abs(suma - resta);
+}
+
+diagonalDifference([
+  [11, 2, 4],
+  [4, 5, 6],
+  [10, 8, -12],
+]);
