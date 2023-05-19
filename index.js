852f461124e7dcff306301f81415af068f204459
Facundo Vergara <fakumax@gmail.com>
2023-05-19 18:43:17 -0300

    Add plusminus

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import { diagonalDifference } from './src/diagonalDifference.js';
+import plusMinus from './src/plusMinus.js';
 
-diagonalDifference([
-  [11, 2, 4],
-  [4, 5, 6],
-  [10, 8, -12],
-]);
+// diagonalDifference([
+//   [11, 2, 4],
+//   [4, 5, 6],
+//   [10, 8, -12],
+// ]);
+
+plusMinus([-4, 3, -9, 0, 4, 1]);
